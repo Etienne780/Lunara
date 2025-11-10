83e7e23c983f7db8fd6bb3a3c080592df31a3d16
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2025-11-10 20:15:20 +0100

    feat: Vertex class

---
+++ SDLCoreLib/include/SDLCoreLib/types/Vertex.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <SDL3/SDL.h>
+#include <CoreLib/Math/Vector2.h>
+#include <CoreLib/Math/Vector4.h>
+
+namespace SDLCore {
+
+    class Vertex {
+    public:
+        Vector2 position;       /**< Vertex position, in SDL_Renderer coordinates  */
+        Vector4 color;          /**< Vertex color (0-255)*/
+        Vector2 texCoordinate;  /**< Normalized texture coordinates, if needed */
+
+        Vertex() = default;
+        Vertex(const Vector2& pos);
+        Vertex(const Vector2& pos, const Vector4& col);
+        Vertex(const Vector2& pos, const Vector4& col, const Vector2& tex);
+
+        /**
+        * @brief Conversion from SDL_Vertex to Vertex.
+        */
+        Vertex(const SDL_Vertex& sdlV);
+
+        /**
+        * @brief Implicit conversion to SDL_Vertex for direct SDL compatibility.
+        */
+        operator SDL_Vertex() const noexcept;
+    };
+
+}
---
+++ SDLCoreLib/src/SDLCoreLib/types/Vertex.cpp
@@ -0,0 +1,41 @@
+#include <CoreLib/Math/MathUtils.h>
+#include "types/Vertex.h"
+
+namespace SDLCore {
+
+
+    Vertex::Vertex(const Vector2& pos)
+        : position(pos) {
+    }
+
+    Vertex::Vertex(const Vector2& pos, const Vector4& col)
+        : position(pos), color(col) {
+    }
+
+    Vertex::Vertex(const Vector2& pos, const Vector4& col, const Vector2& tex)
+        : position(pos), color(col), texCoordinate(tex) {
+    }
+
+    /**
+    * @brief Conversion from SDL_Vertex to Vertex.
+    */
+    Vertex::Vertex(const SDL_Vertex& sdlV)
+        : position(sdlV.position.x, sdlV.position.y),
+        color(sdlV.color.r, sdlV.color.g, sdlV.color.b, sdlV.color.a),
+        texCoordinate(sdlV.tex_coord.x, sdlV.tex_coord.y) {
+    }
+    
+    Vertex::operator SDL_Vertex() const noexcept {
+        SDL_Vertex v{};
+        v.position = SDL_FPoint{ position.x, position.y };
+        v.color = SDL_FColor{
+            MathUtil::ClampValue(color.x, 0, 255),
+            MathUtil::ClampValue(color.y, 0, 255),
+            MathUtil::ClampValue(color.z, 0, 255),
+            MathUtil::ClampValue(color.w, 0, 255)
+        };
+        v.tex_coord = SDL_FPoint{ texCoordinate.x, texCoordinate.y };
+        return v;
+    }
+
+}
